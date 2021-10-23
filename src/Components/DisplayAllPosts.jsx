ae920834139dd0a1b5cedb8dac4b831efbb8b75d
Xúlio Zé <arinspunk@gmail.com>
2021-10-23 22:48:51 +0200

    Update DisplayAllPosts.jsx
    - Import "useRef" React hook
    - Initialize our useRef
    - Create "allPosts", a new state variable
    - Create savePost function
    - Passed down the refs to CreateNewPost component as props

--- src/Components/DisplayAllPosts.jsx
+++ src/Components/DisplayAllPosts.jsx
@@ -1,27 +1,38 @@
-import React, { useState } from 'react';
-import CreateNewPost from './CreateNewPost';
-
+import React, { useState, useRef } from "react";
+import CreateNewPost from "./CreateNewPost";
 const DisplayAllPosts = () => {
-
     const [title, setTitle] = useState("");
     const [content, setContent] = useState("");
+    const [allPosts, setAllPosts] = useState([]);
+    // Initialize useRef
+    const getTitle = useRef();
+    const getContent = useRef();
 
     const savePostTitleToState = event => {
         setTitle(event.target.value);
-        console.log(title);
     };
     const savePostContentToState = event => {
         setContent(event.target.value);
-        console.log(content);
     };
-    
+
+    const savePost = event => {
+        event.preventDefault();
+        setAllPosts([...allPosts, { title, content }]);
+        console.log(allPosts);
+        getTitle.current.value = "";
+        getContent.current.value = "";
+    };
+
     return (
         <>
             <CreateNewPost
                 savePostTitleToState={savePostTitleToState}
                 savePostContentToState={savePostContentToState}
+                getTitle={getTitle}
+                getContent={getContent}
+                savePost={savePost}
             />
         </>
-    )
+    );
 };
 export default DisplayAllPosts;
