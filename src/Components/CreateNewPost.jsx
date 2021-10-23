6d144c52f87820d15b1f5a72ec75b03dd8e73126
Xúlio Zé <arinspunk@gmail.com>
2021-10-23 22:55:35 +0200

    Update CreateNewPost.jsx
    - Use useRef to locate our input field on the DOM
    - Trigger the savePost function onSubmit

--- src/Components/CreateNewPost.jsx
+++ src/Components/CreateNewPost.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const CreateNewPost = props => {
     return (
         <>
-            <form>
+            <form onSubmit={props.savePost}>
                 <h1>Create New Post</h1>
                 <input
                     type="text"
@@ -10,6 +10,7 @@ const CreateNewPost = props => {
                     placeholder="title"
                     size="39"
                     required
+                    ref={props.getTitle}
                 ></input>
                 <br />
                 <br />
@@ -19,6 +20,7 @@ const CreateNewPost = props => {
                     rows="8"
                     cols="41"
                     required
+                    ref={props.getContent}
                 ></textarea>
                 <br />
                 <br />
